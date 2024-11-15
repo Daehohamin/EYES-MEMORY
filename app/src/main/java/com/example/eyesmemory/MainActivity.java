214720ccaac05006983b9a677ec4ddeedfd14627
domong <domong@cau.ac.kr>
2024-11-15 18:45:51 +0900

    내 정보 추가

--- app/src/main/java/com/example/eyesmemory/MainActivity.java
+++ app/src/main/java/com/example/eyesmemory/MainActivity.java
@@ -44,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
         btnNavigateToLogin.setOnClickListener(v -> handleLoginLogout());
 
         btnMyProfile.setOnClickListener(v -> {
-            Toast.makeText(this, "내 정보 기능은 아직 구현되지 않았습니다.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, MyInformActivity.class);
+            startActivity(intent);
         });
 
         btnBrainGame.setOnClickListener(v -> {
