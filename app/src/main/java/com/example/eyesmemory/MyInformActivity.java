4c9bad47147caacd7fc6c393504001675061bfbc
domong <domong@cau.ac.kr>
2024-12-05 00:12:05 +0900

    계정탈퇴 짤림 이슈 해결

--- app/src/main/java/com/example/eyesmemory/MyInformActivity.java
+++ app/src/main/java/com/example/eyesmemory/MyInformActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -44,8 +44,8 @@ public class MyInformActivity extends AppCompatActivity {
             showUserIdDialog();
         }
 
-        ImageButton logoutButton = findViewById(R.id.logout);
-        ImageButton accountDropButton = findViewById(R.id.accountdrop);
+        Button logoutButton = findViewById(R.id.logout);
+        Button accountDropButton = findViewById(R.id.accountdrop);
 
         logoutButton.setOnClickListener(v -> handleLogout());
         accountDropButton.setOnClickListener(v -> handleAccountDrop());
